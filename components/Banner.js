3c9dc91a6d21bbbcd466be7690fddaaa7715f493
Sanjoy Sarker <sanjoysarker157@gmail.com>
2022-01-27 19:32:04 +0600

    initial commit

---
+++ components/Banner.js
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <div className='relative h-[300px] sm:h-[350px] lg:h-[450px] xl:h-[550px] 2xl:h-[650px]'>
+           <Image 
+           src="/banner11.jpg"
+           layout='fill'
+           objectFit='cover'
+           />
+           <div className="absolute top-1/2 w-full text-center">
+               <p className="tex-sm sm:text-2xl font-semibold">Not sure where to go? Perfect.</p>
+               <button className="text-purple-500 bg-white px-10 py-4 rounded-full shadow-md my-3 font-bold hover:shadow-xl active:scale-90 transition duration-150">I'm flexible</button>
+           </div>
+        </div>
+    )
+}
+
+export default Banner
---
+++ components/Footer.js
@@ -0,0 +1,49 @@
+
+
+const Footer = () => {
+    return (
+        <div className="bg-gray-100 text-gray-600">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-y-10 px-32 py-14">
+                <div className="space-y-4 text-xs text-gray-800">
+                    <h5 className="font-bold cursor-pointer text-lg">ABOUT</h5>
+                    <p className="cursor-pointer">Now Airbnb works</p>
+                    <p className="cursor-pointer">Newsroom</p>
+                    <p className="cursor-pointer">Investors</p>
+                    <p className="cursor-pointer">Airbnb Plus</p>
+                    <p className="cursor-pointer">Airbnb Luxe</p>
+                </div>
+                <div className="space-y-4 text-xs text-gray-800">
+                    <h5 className="font-bold cursor-pointer text-lg">PAGES</h5>
+                    <p className="cursor-pointer">Home Page</p>
+                    <p className="cursor-pointer">Features</p>
+                    <p className="cursor-pointer">Contact Us</p>
+                    <p className="cursor-pointer">Pricing</p>
+                    <p className="cursor-pointer">Team Members</p>
+                </div>
+                <div className="space-y-4 text-xs text-gray-800">
+                    <h5 className="font-bold cursor-pointer text-lg">COMMUNITY</h5>
+                    <p className="cursor-pointer">Accessibility</p>
+                    <p className="cursor-pointer">Newsroom</p>
+                    <p className="cursor-pointer">Investors</p>
+                    <p className="cursor-pointer">Airbnb Plus</p>
+                    <p className="cursor-pointer">Airbnb Luxe</p>
+                </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                    <h5 className="font-bold cursor-pointer text-lg">SUPPORT</h5>
+                    <p className="cursor-pointer">Help Center</p>
+                    <p className="cursor-pointer">Trust & Safety</p>
+                    <p className="cursor-pointer">Say Hi YouTube</p>
+                    <p className="cursor-pointer">Easter Eggs</p>
+                    <p className="cursor-pointer">For the win</p>
+                </div>
+            
+                
+            </div>
+            <div className="text-center pb-10 text-md text-sm">
+                Airbnb App | Next js | By Sanjoy Sarker
+            </div>
+        </div>
+    )
+}
+
+export default Footer
